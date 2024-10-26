08955008655193aa0aefad06acd57e1ea0896a25
Suleiman <smjaber05@icloud.com>
2024-10-26 22:03:59 +0400

    Testing Branching

--- Code/BattleShip.c
+++ Code/BattleShip.c
@@ -1,3 +1,42 @@
 int main()
 {
+    printf("hello world");
 }
+
+/*
+void DisplayRules();
+int StartLocalGame();
+
+Main:
+
+Get input
+Setup Grid
+
+while(true){
+    *Display Grid*
+    *Ask for a Move*
+    *Do move (update the grid) (Phase 1.2)*
+    if(PlayerOpponent.shipsRemaining == 0){
+        return 1
+    }
+}
+
+Struct Player:
+    Ship fleet[4]:
+        ship(1)
+        ship(2)
+        ship(3)
+        ship(4)
+
+    int[][] grid
+    int shipsRemaining
+
+Struct Coordinate:
+    int x
+    int y
+
+Struct ship:
+Coorinates positions[]
+int hits
+
+*/
